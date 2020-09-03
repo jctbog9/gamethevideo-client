68b4c1a5a6fcf4ffd9d6016c51abc3e7cb3e311e
John <jtrocchi1@gmail.com>
2020-09-03 16:06:29 -0400

    add initial desktop header

---
+++ components/DesktopHeader.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="header-container">
+    <div class="left-nav">
+      <div class="menu-item">
+        Game The Video
+      </div>
+    </div>
+    <div class="right-nav">
+      <div class="menu-item">
+        Reviews
+      </div>
+      <div class="menu-item">
+        News
+      </div>
+      <div class="menu-item">
+        Blog
+      </div>
+      <div class="menu-item">
+        Gallery
+      </div>
+      <div class="menu-item">
+        About Us
+      </div>
+      <div class="menu-item">
+        Contact
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderContainer'
+}
+</script>
+
+<style>
+.header-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 85px;
+  width: 100%;
+  background-color: #A92728;
+}
+
+.right-nav {
+  display: flex;
+  flex-direction: row;
+}
+
+.menu-item {
+  display: flex;
+  height: 65px;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 20px;
+  color: #ffffff;
+  font-weight: bold;
+}
+</style>
